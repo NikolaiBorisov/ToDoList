54c5d48b40f8d8dcb94a7bf0df8cc1d614bc4572
NIKOLAI <borisov_n_88@mail.ru>
2021-03-25 11:03:11 +0300

    Initial Commit

---
+++ ToDoList_CoreData/CoreDataItems/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoList_CoreData
+//
+//  Created by NIKOLAI BORISOV on 24.03.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ ToDoList_CoreData/CoreDataItems/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  ToDoList_CoreData
+//
+//  Created by NIKOLAI BORISOV on 24.03.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
